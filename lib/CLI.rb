d3887647a8673bf0fd7a6d72d3e2a792179c8c93
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-08-04 18:03:43 +0000

    Added LIB folder.

---
+++ lib/CLI.rb
@@ -0,0 +1,12 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+require_relative "./version"
+require_relative "./curling_iron_review_cli"
+require_relative "./scraper"
+
+
+module CurlingIronReview
+  # Your code goes here...
+end
